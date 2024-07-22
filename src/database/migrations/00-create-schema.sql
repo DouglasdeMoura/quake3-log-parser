d813ed58364ac6b5eb034865103032c1dee82592
Douglas Moura <douglas.ademoura@gmail.com>
2024-07-22 01:05:52 -0300

    feat: remove database

--- src/database/migrations/00-create-schema.sql
+++
@@ -1,8 +0,0 @@
--- Games table to store information about each game session
-CREATE TABLE games (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    total_kills INTEGER NOT NULL DEFAULT 0,
-    players JSON NOT NULL DEFAULT '[]',
-    kills JSON NOT NULL DEFAULT '{}',
-    kills_by_means JSON NOT NULL DEFAULT '{}'
-);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,68 @@
-import { db } from './database/client.js'
+import readline from 'node:readline'
+import fs from 'node:fs'
 
-const statement = db.prepare(`SELECT json_object(
-  'game_' || id, json_object(
-    'total_kills', total_kills,
-    'players', json(players),
-    'kills', json(kills),
-    'kills_by_means', json(kills_by_means)
-  )
-) AS response
-FROM games;`)
+const log = fs.createReadStream('./src/database/qgames.txt')
 
-const games = statement.all().map((item) => JSON.parse(item.response))
+const rl = readline.createInterface({
+  input: log,
+  crlfDelay: Infinity,
+})
 
-console.log(JSON.stringify(games, null, 2))
+let totalKills = 0
+let players = []
+let kills = {}
+let killsByMeans = {}
+const games = []
+
+rl.on('line', async (line) => {
+  if (line.includes('InitGame')) {
+    // Reset all variables
+    totalKills = 0
+    players = []
+    kills = {}
+    killsByMeans = {}
+  }
+
+  if (line.includes('ShutdownGame')) {
+    games.push({
+      total_kills: totalKills,
+      players: Array.from(new Set([...players])),
+      kills,
+      kills_by_means: killsByMeans,
+    })
+  }
+
+  if (line.includes('ClientUserinfoChanged')) {
+    const player = line.split('n\\')[1].split('\\t')[0]
+    players.push(player)
+  }
+
+  if (line.includes('Kill')) {
+    totalKills += 1
+
+    const [part1, part2] = line.split('Kill:')[1].split('killed')
+    const killer = part1.trim().split(': ')[1].trim()
+    const [killed, means] = part2.trim().split('by').map((item) => item.trim())
+
+    if (killer === '<world>') {
+      kills[killed] = (kills[killed] || 0) - 1
+    } else {
+      kills[killer] = (kills[killer] || 0) + 1
+    }
+
+    killsByMeans[means.trim()] = (killsByMeans[means.trim()] || 0) + 1
+  }
+})
+
+rl.on('close', () => {
+  const data = {}
+  games.forEach((game, index) => {
+    data['game_' + (index + 1)] = {
+      total_kills: game.total_kills,
+      players: game.players,
+      kills: game.kills,
+      kills_by_means: game.kills_by_means,
+    }
+  })
+  console.log(data)
+})
