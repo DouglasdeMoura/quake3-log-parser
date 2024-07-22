84a4f9e76ce33e8ce1f5cad82f3ba7d303959689
Douglas Moura <douglas.ademoura@gmail.com>
2024-07-22 00:14:54 -0300

    feat: process log

--- src/database/seed.js
+++ src/database/seed.js
@@ -8,7 +8,51 @@ const rl = readline.createInterface({
   crlfDelay: Infinity,
 })
 
+let totalKills = 0
+let players = []
+let kills = {}
+let killsByMeans = {}
+
 // Process each line
 rl.on('line', async (line) => {
-  console.log(line)
+  if (line.includes('InitGame')) {
+    console.log('Game started')
+    console.log('')
+
+    totalKills = 0
+    players = []
+    kills = {}
+    killsByMeans = {}
+  }
+
+  if (line.includes('ShutdownGame')) {
+    console.log('Game ended')
+
+    console.log('Total kills:', totalKills)
+    console.log('Players:', Array.from(new Set([...players])))
+    console.log('Kills:', kills)
+    console.log('Kills by means:', killsByMeans)
+    console.log('-----------------------------------')
+  }
+
+  if (line.includes('ClientUserinfoChanged')) {
+    const player = line.split('n\\')[1].split('\\t')[0]
+    players.push(player)
+  }
+
+  if (line.includes('Kill')) {
+    totalKills += 1
+
+    const [part1, part2] = line.split('Kill:')[1].split('killed')
+    const killer = part1.trim().split(': ')[1].trim()
+    const [killed, means] = part2.trim().split('by').map((item) => item.trim())
+
+    if (killer === '<world>') {
+      kills[killed] = (kills[killed] || 0) - 1
+    } else {
+      kills[killer] = (kills[killer] || 0) + 1
+    }
+
+    killsByMeans[means.trim()] = (killsByMeans[means.trim()] || 0) + 1
+  }
 })
